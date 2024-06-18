455d893d68463b9d51ec4bcf2037f2e917f09390
Ottar Kvindesland <149434896+LA9IHA@users.noreply.github.com>
2024-06-18 16:30:52 +0200

    Add files via upload

--- PiwigoServer/genExcel.py
+++ PiwigoServer/genExcel.py
@@ -4,6 +4,8 @@ import datetime
 import os
 import shutil
 import hashlib
+import pandas as pd
+import fnmatch
 
 import mysql.connector
 #pip3 install mysql-connector-python
@@ -16,7 +18,7 @@ from os import listdir
 # Licence: GNU 2.0
 # Copyright: Ottar Kvindesland, 2024
 
-class extractTables ():
+class genExcels ():
 
     def __init__(self):
         super().__init__()
@@ -25,83 +27,29 @@ class extractTables ():
         self.host='localhost'
         self.user='ottar'
         self.password='ottar'
+        self.csvDir='/home/ottar/'
             
-        tables = self.getAllTableNames()
+        tables = self.getCSVfiles()
         for table in tables:
-            self.dumpTable (table)
-        
-        #picfile = self.subdir + self.fInputPic
-        #self.pic_wb = load_workbook(filename=picfile)
-        #self.wp = self.pic_wb.worksheets[0]
-        
-        #self.checkPics()
-        #self.pic_wb.save(self.subdir + self.fOutputPic)
+            self.genXlsx (table)
                 
-    def getAllTableNames(self):
+    def getCSVfiles(self):
         
-        conn = mysql.connector.connect(
-            host=self.host,
-            user=self.user,
-            password=self.password,
-            database=self.dbName
-            )
-        cursor = conn.cursor()
-        cursor.execute('show tables')
-        results = cursor.fetchall()
+        results = [file for file in os.listdir(self.csvDir) if fnmatch.fnmatch(file, '*.csv')]
         r = []
         for row in results:
             r.append(row)
             
         return r
 
-    def dumpTable (self, tableName):
+    def genXlsx (self, tableName):
         
-        table = tableName[0]
-        
-        print ('Printer: ', table)
+        table = tableName[0]      
+        if os.path.exists(file_path):
+	        df = pd.read_csv(self.csvDir + tableName)
+	        df.to_excel(self.csvDir + tableName, index=False)
+        	os.remove(self.csvDir + tableName)
 
-        c1 = "SET @col_names = (SELECT GROUP_CONCAT(CONCAT('\\'', COLUMN_NAME, '\\'')) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + table + "' AND TABLE_SCHEMA = '" + self.dbName + "');"
-        c2 = "SET @sql = CONCAT('SELECT ', @col_names, ' UNION ALL SELECT * FROM " + table + " INTO OUTFILE \\'" + table + ".csv\\' FIELDS TERMINATED BY \\\',\\\' ENCLOSED BY \\\'\"\\\' LINES TERMINATED BY \\\'\n\\\'');"
-        c3 = 'PREPARE stmt FROM @sql;'
-        c4 = 'EXECUTE stmt;'
-        c5 = 'DEALLOCATE PREPARE stmt;'
-
-        conn = mysql.connector.connect(
-            host=self.host,
-            user=self.user,
-            password=self.password,
-            database=self.dbName
-            )
-        cursor = conn.cursor()
-        cursor.execute(c1)
-        cursor.execute(c2)
-        cursor.execute(c3)
-        cursor.execute(c4)
-        cursor.execute(c5)
-        
-    def checkPics(self):
-        n = 0
-        c = self.cpParentDoc+1
-        bi = self.cpBareItem+1
-        fn = self.cpFileName+1
-        err = self.cpFileError+1
-        for pic in self.wp.iter_rows(min_row=1, max_row=self.wp.max_row, min_col=1, max_col=self.cpLastPic, values_only=False):
-            n+=1
-            parent = self.wp.cell(row=n, column=c).value
-            bareItem = self.wp.cell(row=n, column=bi).value
-            fileName = self.wp.cell(row=n, column=fn).value
-            fpath = self.subdir + 'PHOTOS/' + bareItem + '/' + bareItem + '/'
-            fthumb = fpath + 'doc_pic.jpg'
-            ffull = fpath + fileName
-            
-            if (bareItem.lower() != 'name'):
-                if not(self.testFile(fpath)):
-                    self.wp.cell(row=n, column=self.cpFileError).value = 'File Error'
-                    print('ERROR: ' + fpath)
-                
-        
-
-  
-a = extractTables()
+a = genExcels()
 
 
