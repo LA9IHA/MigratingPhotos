ce4e60263477e75033b68fce79cd3339a9073e86
Ottar Kvindesland <149434896+LA9IHA@users.noreply.github.com>
2024-06-19 17:51:02 +0200

    Add files via upload

--- PiwigoServer/serverExtract.py
+++ PiwigoServer/serverExtract.py
@@ -1,17 +1,23 @@
+# See ../highstage/cols.py header for info.
+# (C) 2024: Ottar Kvindesland, Licence: GPL 2.0
+# Purpose: Export from HighStage album to Piwigo Fils structure. Build metadata from items
+
+
+
 import datetime
-#import openpyxl
-#from openpyxl import Workbook
 import os
 import shutil
 import hashlib
 import pandas as pd
 import fnmatch
 import glob
+import zipfile
 
 import mysql.connector
 
 from openpyxl.descriptors.base import DateTime
 from openpyxl.reader.excel import load_workbook
+from openpyxl import Workbook
 from os import listdir
 
 # Purpose: Check the integrity of the media files
@@ -29,16 +35,18 @@ class extractTables ():
         self.password='secretPassword'
         self.migrateDir = '/tmp/'
             
-        self.delTables()
         tables = self.getAllTableNames()
+        zipFiles = []
+        print ('Extracting tables\nWhen complete run the script lines below:\n\n')
         for table in tables:
-            self.dumpTable (table)
+            self.dumpTable (table[0])
+            self.genXlsx (table[0])
+            zipFiles.add(table[0] + '.xlsx')
+            
+        with zipfile.ZipFile('dbdump.zip', 'w') as dumpzip:
+            for fileName in zipFiles:
+                dumpzip.write(fileName)
         
-        tables = self.getCSVfiles()
-        for table in tables:
-            self.genXlsx (table)
-        
-                
     def getAllTableNames(self):
         
         conn = mysql.connector.connect(
@@ -56,12 +64,8 @@ class extractTables ():
             
         return r
 
-    def dumpTable (self, tableName):
+    def dumpTable (self, table):
         
-        table = tableName[0]
-        
-        print ('Printer: ', table)
-
         c1 = "SET @col_names = (SELECT GROUP_CONCAT(CONCAT('\\'', COLUMN_NAME, '\\'')) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + table + "' AND TABLE_SCHEMA = '" + self.dbName + "');"
         c2 = "SET @sql = CONCAT('SELECT ', @col_names, ' UNION ALL SELECT * FROM " + table + " INTO OUTFILE \\'" +  self.migrateDir + table + ".csv\\' FIELDS TERMINATED BY \\\',\\\' ENCLOSED BY \\\'\"\\\' LINES TERMINATED BY \\\'\n\\\'');"
         c3 = 'PREPARE stmt FROM @sql;'
@@ -93,17 +97,22 @@ class extractTables ():
     def genXlsx (self, tableName):
         
         table = tableName[0]
-        csvFile = ''
-        if os.path.exists(self.migrateDir):
-            csvFile = self.migrateDir + tableName
-            df = pd.read_csv(csvFile)
-            df.to_excel(self.migrateDir + tableName + '.xlsx', index=False)
-            #os.remove(csvFile)
-            
-    def delTables(self):
-        csv_files = glob.glob(self.migrateDir + '*.csv')
-        for file in csv_files:
-            os.remove(file)
+        csvFileName = self.migrateDir + tableName + '.csv'
+        wb = Workbook()
+        ws = wb.active
+        r = 1
+        print ('sudo rm ' + csvFileName )
+        with open(csvFileName, 'r') as csvFile:
+            for line in csvFile:
+                line = line.strip().replace(',\\N', ',""')
+                elements = line.split('","')
+                c = 1
+                for element in elements:
+                    element = element.strip('"')
+                    ws.cell(row=r, column=c).value = element
+                    c += 1
+                r += 1
+        wb.save(tableName + '.xlsx')
   
 a = extractTables()
 
